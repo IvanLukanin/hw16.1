83307047e61749c73f56e91fd893f4f9e0a68c2d
IVAN <luka.news.2001@mail.ru>
2023-06-16 12:36:04 +0300

    Spring - 1

---
+++ src/main/java/com/example/hw18_1/Hw181Application.java
@@ -0,0 +1,13 @@
+package com.example.hw18_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw181Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw181Application.class, args);
+    }
+
+}
