da11e39caa433aa6869fdfe909302b26f5e834f3
IVAN <luka.news.2001@mail.ru>
2023-06-16 12:39:23 +0300

    Spring - 1

---
+++ src/test/java/com/example/hw18_1/Hw181ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw18_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw181ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
